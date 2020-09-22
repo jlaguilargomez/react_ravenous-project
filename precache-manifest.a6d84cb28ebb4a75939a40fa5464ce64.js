a2ee01b1b833414eea8ec5337c3700a107687ce2
jlaguilargomez <jlaguigom@gmail.com>
2020-09-22 17:50:16 +0200

    Updates

---
+++ precache-manifest.a6d84cb28ebb4a75939a40fa5464ce64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c049f48a7c420052c7d24e571a1113",
+    "url": "/react_ravenous-project/index.html"
+  },
+  {
+    "revision": "b78f71ebf8a6a9a249be",
+    "url": "/react_ravenous-project/static/css/main.f2ba9b05.chunk.css"
+  },
+  {
+    "revision": "44b17a0c356c7e732238",
+    "url": "/react_ravenous-project/static/js/2.4f2af0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_ravenous-project/static/js/2.4f2af0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78f71ebf8a6a9a249be",
+    "url": "/react_ravenous-project/static/js/main.3c892215.chunk.js"
+  },
+  {
+    "revision": "e6b78f178d9011456fdb",
+    "url": "/react_ravenous-project/static/js/runtime-main.285aa39f.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/react_ravenous-project/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/react_ravenous-project/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
