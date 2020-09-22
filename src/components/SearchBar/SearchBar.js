55526bb71ff833bb0844e9811457669760e63b16
jlaguilargomez <jlaguigom@gmail.com>
2020-09-22 17:18:50 +0200

    Ravenous part 3 /1

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,37 +1,57 @@
 import React from 'react';
 import './SearchBar.css'
 
-const SORT_BY_OPTIONS = {
-  bestMatch: 'best_match',
-  highestRated: 'rating',
-  mostReviewed: 'review_count',
-};
+// const SORT_BY_OPTIONS = {
+ 
+// };
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+
+    this.sortByOptions = { bestMatch: 'best_match', highestRated: 'rating', mostReviewed: 'review_count' };
+  }
+
+  getSortByClass(shortByOption) {
+    return this.state.sortBy === shortByOption ? 'active' : '';
+  }
+
+  handleSortByChange(shortByOption) {
+    this.setState({
+      sortBy: shortByOption,
+    });
+  }
   renderSortByOptions() {
-    return Object.keys(SORT_BY_OPTIONS).map((sortByOption) => {
-      let sortByOptionValue = SORT_BY_OPTIONS[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>; 
+    return Object.keys(this.sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (
+        <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+          {sortByOption}
+        </li>
+      );
     });
   }
 
   render() {
     return (
-      <section className="SearchBar">
-        <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOptions()}
-          </ul>
+      <section className='SearchBar'>
+        <div className='SearchBar-sort-options'>
+          <ul>{this.renderSortByOptions()}</ul>
         </div>
-        <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+        <div className='SearchBar-fields'>
+          <input placeholder='Search Businesses' />
+          <input placeholder='Where?' />
         </div>
-        <div className="SearchBar-submit">
-          <a href="#">Let's Go</a>
+        <div className='SearchBar-submit'>
+          <a href='#'>Let's Go</a>
         </div>
       </section>
-    )
+    );
   }
 }
 
